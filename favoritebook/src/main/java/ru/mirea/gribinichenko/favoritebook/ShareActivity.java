205a031a0a25508012358bfdf40d260b0115c2af
Glo_Murlik <gribinichenko.danil@yandex.ru>
2023-09-12 13:59:46 +0300

    _2_

---
+++ favoritebook/src/main/java/ru/mirea/gribinichenko/favoritebook/ShareActivity.java
@@ -0,0 +1,38 @@
+package ru.mirea.gribinichenko.favoritebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import ru.mirea.gribinichenko.favoritebook.R;
+
+public class ShareActivity extends AppCompatActivity {
+
+    EditText textBook;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+        textBook = findViewById(R.id.enterNameFavBook);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            TextView ageView = findViewById(R.id.textViewBook);
+            String university = extras.getString(MainActivity.KEY);
+            ageView.setText(String.format("Любимая книга разработчика: %s", university));
+        }
+    }
+
+    public void enterButton(View view) {
+        String text ="Ваша любимая книга: " + textBook.getText();
+        Intent data = new Intent();
+        data.putExtra(MainActivity.USER_MESSAGE, text);
+        setResult(Activity.RESULT_OK, data);
+        finish();
+    }
+}
